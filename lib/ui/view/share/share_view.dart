5cb73f8aa86014b2cfeea2e2675a876a58beb8af
wlgggy <doyouregretme@naver.com>
2025-06-05 19:07:20 +0900

    ★[create] : share_view project★

---
+++ lib/ui/view/share/share_view.dart
@@ -0,0 +1,16 @@
+import 'package:drivemate/ui/widget/sized_widget.dart';
+import 'package:flutter/material.dart';
+
+class Share extends StatefulWidget {
+  const Share({super.key});
+
+  @override
+  State<StatefulWidget> createState() => ShareState();
+}
+
+class ShareState extends State<Share> {
+  @override
+  Widget build(BuildContext context) {
+    return Material(child: FullSizedBox());
+  }
+}
